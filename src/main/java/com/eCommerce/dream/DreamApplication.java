14768f2657c547974279a1c77d69b07fb6b4925c
Welson Junior <welsongt9@live.com>
2023-02-07 02:20:04 -0300

    Funciona caralho 3

--- src/main/java/com/eCommerce/dream/DreamApplication.java
+++ src/main/java/com/eCommerce/dream/DreamApplication.java
@@ -8,7 +8,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@Profile("test")
 public class DreamApplication {
 
 	public static void main(String[] args) {
